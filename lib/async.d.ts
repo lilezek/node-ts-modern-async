4f477287f2626b533a54fdf6490b8d703935074f
ezequiel <ezequiel.santamaria@um.es>
2017-06-27 12:11:10 +0200

    First verison.

---
+++ lib/async.d.ts
@@ -0,0 +1,3 @@
+export declare function wait(ms: number): Promise<{}>;
+export declare function nextTick(): Promise<void>;
+export declare function map<T = any, V = any>(arr: T[], cb: (element: T) => Promise<V>): Promise<V[]>;
---
+++ src/async.ts
@@ -0,0 +1,17 @@
+export async function wait(ms: number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export async function nextTick() {
+  await wait(0);
+}
+
+export async function map<T = any, V = any>(arr: T[], cb: (element: T) => Promise<V>) {
+  const result: V[] = [];
+  for (const el of arr) {
+    result.push(await cb(el));
+  }
+  return result;
+}
