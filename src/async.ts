90bf4f075b4c16821dd43f7e3d1e026bcb681db1
ezequiel <ezequiel.santamaria@um.es>
2017-06-27 13:26:23 +0200

    Added: async array.

--- src/async.ts
+++ src/async.ts
@@ -21,3 +21,28 @@ export async function immediate() {
     resolve();
   });
 }
+
+export class AsyncArray<T> extends Array<T> {
+
+  private consumers: Array<(v: T) => {}> = [];
+
+  public async consume() {
+    return new Promise<T>((resolve) => {
+      const element = this.shift();
+      if (element) {
+        resolve(element);
+      } else {
+        this.consumers.push(resolve as any);
+      }
+    });
+  }
+
+  public produce(v: T) {
+    const consumer = this.consumers.shift();
+    if (consumer) {
+      consumer(v);
+      return 0;
+    }
+    return this.push(v);
+  }
+}
