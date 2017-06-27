c9c4d4a3fbbbeb436581c3050131a549980435e0
ezequiel <ezequiel.santamaria@um.es>
2017-06-27 13:26:39 +0200

    Fix: compilation.

--- lib/async.d.ts
+++ lib/async.d.ts
@@ -2,3 +2,8 @@ export declare function wait(ms: number): Promise<{}>;
 export declare function nextTick(): Promise<void>;
 export declare function map<T = any, V = any>(arr: T[], cb: (element: T) => Promise<V>): Promise<V[]>;
 export declare function immediate(): Promise<{}>;
+export declare class AsyncArray<T> extends Array<T> {
+    private consumers;
+    consume(): Promise<T>;
+    produce(v: T): number;
+}
