052ab080af7dbf8c49ac763c58bc5c14dbaa4bed
ezequiel <ezequiel.santamaria@um.es>
2017-07-13 10:38:29 +0200

    Compiled.

--- lib/async.d.ts
+++ lib/async.d.ts
@@ -2,6 +2,7 @@ export declare function wait(ms: number): Promise<{}>;
 export declare function nextTick(): Promise<void>;
 export declare function map<T = any, V = any>(arr: T[], cb: (element: T) => Promise<V>): Promise<V[]>;
 export declare function immediate(): Promise<{}>;
+export declare function forEach<T = any>(iterable: ArrayLike<T>, cb: (element: T) => Promise<void>): Promise<void>;
 export declare class AsyncArray<T> extends Array<T> {
     private consumers;
     consume(): Promise<T>;
